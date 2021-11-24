b4d822b5f1770798b157f4c6b942586536e06150
Alex Latz <alexlatz@westminster.net>
2021-11-24 14:23:08 -0500

    more progress on the checkers

--- Checkers/MCTS/Board.cpp
+++ Checkers/MCTS/Board.cpp
@@ -6,6 +6,12 @@ Board::Board(vector<string>& board, char turn) {
     prepareBoard();
 }
 
+Board::Board(vector<string>& board, char turn, set<pair<int, int> >& pieces) {
+    this->board = board;
+    this->turn = turn;
+    this->pieces = pieces;
+}
+
 char Board::opposite(char c) {
     if (tolower(c) == 'w') return 'b';
     else return 'w';
@@ -16,11 +22,11 @@ bool Board::isUpper(char c) {
 }
 
 void Board::prepareBoard() {
-    vector<pair<int, int> > pieces;
+    set<pair<int, int> > pieces;
     for (int i = 0; i < board.size(); i++) {
         for (int j = 0; j < board[0].size(); j++) {
             if (tolower(board[i][j]) == turn) {
-                pieces.push_back(pair<int, int>(i, j));
+                pieces.insert(pair<int, int>(i, j));
             }
         }
     }
@@ -33,55 +39,105 @@ vector<Board> Board::getChildren() {
         vector<Board> children;
         vector<Move> moves;
         for (pair<int, int> p : pieces) {
-            if (p.first > 0) {
+            int bigOn = -1;
+            if (isUpper(board[p.first][p.second])) bigOn = 0;
+            else if ((p.first-2 == 0 && board[p.first][p.second] == 'w') || (p.first+2 == board.size()-1 && board[p.first][p.second] == 'b')) bigOn = 1;
+            if (p.first > 0 && (isUpper(board[p.first][p.second]) || board[p.first][p.second] == 'w')) {
                 if (p.first > 1 && p.second > 1 && board[p.first-2][p.second-2] == '-' && tolower(board[p.first-1][p.second-1]) == opposite(turn)) {
                     if (!capture) {
                         moves.clear();
                         capture = true;
                     }
-                    moves.push_back(Move(p, pair<int, int>(p.first-2, p.second-2), pair<int, int>(p.first-1, p.second-1)));
+                    if (p.first-2 == 0) bigOn = 1;
+                    moves.push_back(Move(p, pair<int, int>(p.first-2, p.second-2), pair<int, int>(p.first-1, p.second-1), bigOn));
                 }
                 if (p.second > 0 && !capture && board[p.first-1][p.second-1] == '-') {
-                    moves.push_back(Move(p, pair<int, int>(p.first-1, p.second-1)));
+                    if (p.first-2 == 0) bigOn = 1;
+                    moves.push_back(Move(p, pair<int, int>(p.first-1, p.second-1), bigOn));
                 }
                 if (p.first > 1 && p.second < board[0].size()-2 && board[p.first-2][p.second+2] == '-' && tolower(board[p.first-1][p.second+1]) == opposite(turn)) {
                     if (!capture) {
                         moves.clear();
                         capture = true;
                     }
-                    moves.push_back(Move(p, pair<int, int>(p.first-2, p.second+2), pair<int, int>(p.first-1, p.second+1)));
+                    moves.push_back(Move(p, pair<int, int>(p.first-2, p.second+2), pair<int, int>(p.first-1, p.second+1), bigOn));
                 } else if (p.second < board[0].size()-1 && !capture && board[p.first-1][p.second+1] == '-') {
-                    moves.push_back(Move(p, pair<int, int>(p.first-1, p.second+1)));
+                    moves.push_back(Move(p, pair<int, int>(p.first-1, p.second+1), bigOn));
                 }
             }
-            if (p.first < board.size()-1 && isUpper(board[p.first][p.second])) {
+            if (p.first < board.size()-1 && (isUpper(board[p.first][p.second]) || board[p.first][p.second] == 'b')) {
                 if (p.first < board.size()-2 && p.second > 1 && board[p.first+2][p.second-2] == '-' && tolower(board[p.first+1][p.second-1]) == opposite(turn)) {
                     if (!capture) {
                         moves.clear();
                         capture = true;
                     }
-                    moves.push_back(Move(p, pair<int, int>(p.first+2, p.second-2), pair<int, int>(p.first+1, p.second-1)));
+                    moves.push_back(Move(p, pair<int, int>(p.first+2, p.second-2), pair<int, int>(p.first+1, p.second-1), bigOn));
                 } else if (p.second > 0 && !capture && board[p.first+1][p.second-1] == '-') {
-                    moves.push_back(Move(p, pair<int, int>(p.first+1, p.second-1)));
+                    moves.push_back(Move(p, pair<int, int>(p.first+1, p.second-1), bigOn));
                 }
                 if (p.first < board.size()-2 && p.second < board[0].size()-2 && board[p.first+2][p.second+2] == '-' && tolower(board[p.first+1][p.second+1]) == opposite(turn)) {
                     if (!capture) {
                         moves.clear();
                         capture = true;
                     }
-                    moves.push_back(Move(p, pair<int, int>(p.first+2, p.second+2), pair<int, int>(p.first+1, p.second+1)));
+                    moves.push_back(Move(p, pair<int, int>(p.first+2, p.second+2), pair<int, int>(p.first+1, p.second+1), bigOn));
                 } else if (p.second < board[0].size()-1 && !capture && board[p.first+1][p.second+1] == '-') {
-                    moves.push_back(Move(p, pair<int, int>(p.first+1, p.second+1)));
+                    moves.push_back(Move(p, pair<int, int>(p.first+1, p.second+1), bigOn));
                 }
             }
         }
         if (capture) {
+            vector<Move> newMoves;
             for (Move m : moves) {
-                chainMove(m, moves);
+                chainMove(m, newMoves);
             }
+            moves.insert(moves.end(), newMoves.begin(), newMoves.end());
+        }
+        for (Move m : moves) {
+            children.push_back(applyMove(m));
         }
         this->capture = capture;
         this->children = children;
     }
     return children;
+}
+
+ void Board::chainMove(Move m, vector<Move>& moves) {
+    pair<int, int> l  = m.moves[m.moves.size()-1];
+    int bigOn = m.bigOn;
+    if (m.bigOn == -1 && (l.first-2 == 0 && board[m.initialPos.first][m.initialPos.second] == 'w') || (l.first+2 == board.size()-1 && board[m.initialPos.first][m.initialPos.second] == 'b')) {
+        bigOn = m.moves.size()+1;
+    }
+     if (l.first > 1) {
+        if (l.second > 1 && board[l.first-2][l.second-2] == '-' && board[l.first-1][l.second-1] == opposite(board[m.initialPos.first][m.initialPos.second])) {
+            Move newM(m, pair<int, int>(l.first-2, l.second-2), pair<int, int>(l.first-1, l.second-1), bigOn);
+            moves.push_back(newM);
+            chainMove(newM, moves);
+        }
+        if (l.second < board[0].size()-2 && board[l.first-2][l.second+2] == '-' && board[l.first-1][l.second+1] == opposite(board[m.initialPos.first][m.initialPos.second])) {
+            Move newM(m, pair<int, int>(l.first-2, l.second+2), pair<int, int>(l.first-1, l.second+1), bigOn);
+            moves.push_back(newM);
+            chainMove(newM, moves);
+        }
+    }
+    if (l.first < board.size()-2 && isUpper(board[m.initialPos.first][m.initialPos.second])) {
+        if (l.second > 1 && board[l.first+2][l.second-2] == '-' && board[l.first+1][l.second-1] == opposite(board[m.initialPos.first][m.initialPos.second])) {
+            Move newM(m, pair<int, int>(l.first+2, l.second-2), pair<int, int>(l.first+1, l.second-1), bigOn);
+            moves.push_back(newM);
+            chainMove(newM, moves);
+        }
+        if (l.second < board[0].size()-2 && board[l.first+2][l.second+2] == '-' && board[l.first+1][l.second+1] == opposite(board[m.initialPos.first][m.initialPos.second])) {
+            Move newM(m, pair<int, int>(l.first+2, l.second+2), pair<int, int>(l.first+1, l.second+1), bigOn);
+            moves.push_back(newM);
+            chainMove(newM, moves);
+        }
+    }
+}
+
+Board Board::applyMove(Move m) {
+    vector<string> newBoard(board);
+    set<pair<int, int> > newPieces(pieces);
+    char newTurn = opposite(turn);
+    newBoard[m.initialPos.first][m.initialPos.second] = '-';
+    newBoard[m.moves[m.moves.size()-1].first][m.moves[m.moves.size()-1].second] = board[m.initialPos.first][m.initialPos.second];
 }
--- Checkers/MCTS/Board.h
+++ Checkers/MCTS/Board.h
@@ -8,38 +8,47 @@
 using namespace std;
 
 class Board {
-    char turn;
-    bool capture;
-    vector<Board> children;
-    vector<string> board;
-    vector<pair<int, int> > pieces;
-    void prepareBoard();
-    public:
-        Board(vector<string>& board, char turn);
-        Board(vector<string>& board, char turn, vector<pair<int, int> > pieces);
-        vector<Board> getChildren();
-        static char opposite(char c);
-        static bool isUpper(char c);
     struct Move {
+        int bigOn;
         pair<int, int> initialPos;
         vector<pair<int, int> > moves;
         vector<pair<int, int> > removedPieces;
-        Move(pair<int, int> pos, vector<pair<int, int> > moves) {
-            this->initialPos = pos;
-            this->moves = moves;
-        }
-        Move(pair<int, int> initialPos, pair<int, int> move) {
+        Move(pair<int, int> initialPos, pair<int, int> move, int bigOn) {
+            this->bigOn = bigOn;
             this->initialPos = initialPos;
             vector<pair<int, int> > moves;
             moves.push_back(move);
             this->moves;
         }
-        Move(pair<int, int> initialPos, pair<int, int> move, pair<int, int> removedPiece) : Move(initialPos, move) {
+        Move(pair<int, int> initialPos, pair<int, int> move, pair<int, int> removedPiece, int bigOn) : Move(initialPos, move) {
+            this->bigOn = bigOn;
             vector<pair<int, int> > removedPieces;
             removedPieces.push_back(removedPiece);
             this->removedPieces = removedPieces;
         }
+        Move(Move m, pair<int, int> move, pair<int, int> removedPiece, int bigOn) {
+            this->bigOn = bigOn;
+            this->initialPos = m.initialPos;
+            this->moves = m.moves;
+            this->removedPieces = m.removedPieces;
+            this->moves.push_back(move);
+            this->removedPieces.push_back(removedPiece);
+        }
     };
+    char turn;
+    bool capture;
+    vector<Board> children;
+    vector<string> board;
+    set<pair<int, int> > pieces;
+    void prepareBoard();
+    void chainMove(Move m, vector<Move>& moves);
+    Board applyMove(Move m);
+    public:
+        Board(vector<string>& board, char turn);
+        Board(vector<string>& board, char turn, set<pair<int, int> >& pieces);
+        vector<Board> getChildren();
+        static char opposite(char c);
+        static bool isUpper(char c);
 };
 
 
